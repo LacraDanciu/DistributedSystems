7f3f53b27b2226f3321bd8eb014c1b740b4005e4
LacraDanciu <danciu.diana.l@gmail.com>
2017-10-21 20:07:13 +0300

    firstExampleLogin

---
+++ firstExampleLogin/src/model/Authenticator.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Authenticator {
+    public String authenticate(String username, String password) {
+        if (("prasad".equalsIgnoreCase(username))
+                && ("password".equals(password))) {
+            return "success";
+        } else {
+            return "failure";
+        }
+    }
+}
